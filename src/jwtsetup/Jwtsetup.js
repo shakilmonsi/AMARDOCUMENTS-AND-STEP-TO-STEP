3f746027a5b8fca7c7c1157913f784cb276bd06d
Md Shakil monsi <samdshakilofficial@gmail.com>
2023-10-06 20:39:54 +0600

    jsonToken and step to

--- src/jwtsetup/Jwtsetup.js
+++ src/jwtsetup/Jwtsetup.js
@@ -12,6 +12,22 @@ app.get('/jwt', async (req, res) => {
     console.log('jwt data ress',user)
     res.send({accessToken: 'token'})
 })
+{/* .2.stpep send jwt cliend saite */}
+//class -75-4 jsonwebtoken setup 
+app.get('/jwt', async (req, res) => {
+        const email = req.query.email;
+        const query = { email: email };
+    const user= await userCollextion.findOne(query)
+    
+if(user){
+    const token = jwt.sing({email},process.env.ACCESS_TOKEN,{expiresIn:'1h'})
+ return returnres.send({accessToken:token}) 
+
+}
+    console.log('jwt data ress',user)
+    res.status(403).send({accessToken: ''})
+})
+
             
         </div>
     );
